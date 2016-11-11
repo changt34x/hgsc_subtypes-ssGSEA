d9f4f29f86fe1c4bf622bb9180208284420de99c
changt34x <changt34x@gmail.com>
2016-11-11 01:59:11 -0500

    Create aggregate boxplots

--- scripts/ssGSEA.R
+++ scripts/ssGSEA.R
@@ -19,14 +19,16 @@ args <- commandArgs(trailingOnly = T)
 ############################
 library(GSVA)
 library(curatedOvarianData)
+library(ggplot2)
 
 source('scripts/functions/LoadOVCA_Data.R')
+source('scripts/functions/ssGSEA_Aggregate_Table.R')
 
 ############################
 # 2. Load Data
 ############################
 # Load datasets specified in arguments
-ExpData <- LoadOVCA_Data(datasets = args, genelist_subset = 'None')
+ExpData <- LoadOVCA_Data(datasets = args, genelist_subset = 'commongenes')
 
 # Load lm22 gene set
 lm22_genes <- read.table('data/lm22_genes.tsv', sep = '\t', header = T, row.names = 1)
@@ -61,14 +63,54 @@ for (dataset in 1:length(ExpData)) {
   output_dir <- paste0('output/figures/', names(ExpData)[dataset])
   dir.create(output_dir, showWarnings = F)
   
-  for (i in 2:4) {
-    for (j in 5:(dim(cluster_ssgsea)[2])) {
-      png(filename=paste0(output_dir, '/', names(ExpData)[dataset], '_', data_label[j], ' k=',
+  # Create plots of ssGSEA scores by cell type and dataset
+  for (i in 2:(dim(clustering)[2] + 1)) {
+    for (j in (dim(clustering)[2] + 2):(dim(cluster_ssgsea)[2])) {
+      png(filename = paste0(output_dir, '/', names(ExpData)[dataset], '_', data_label[j], ' k=',
                           i, '.png'))
       boxplot(cluster_ssgsea[,j]~cluster_ssgsea[,i], data = cluster_ssgsea, xlab = 'Subtypes',
               ylab = 'Abundance', main = paste0(names(ExpData)[dataset], ' ', data_label[j],
                                                 ' k = ', i))
       dev.off()
     }
+  }
+}
+
+############################
+# 4. Aggregate analysis
+############################
+# Get aggregate dataframe
+agg_data <- ssGSEA_Aggregate_Table(args, dim(clustering)[2])
+
+# Get types of cells analyzed
+cell_types <- unique(agg_data$Cell.Type)
+
+# Create output directory
+dir.create('output/figures/lm22_overall', showWarnings = F)
+
+# Plot data
+for (i in 2:(dim(clustering)[2] + 1)) {
+  # Create output directory by subtyping number
+  output_dir <- paste0('output/figures/lm22_overall/k=', i)
+  dir.create(output_dir, showWarnings = F)
+  
+  for (j in 1:length(cell_types)) {
+    # Get cell type
+    current_type <- cell_types[j]
+    
+    # Select data from table
+    sel <- agg_data$K == i & agg_data$Cell.Type == current_type
+    
+    # Plot and save as
+    ggplot(data = agg_data[sel,], mapping = aes(x = as.character(agg_data$Subtype[sel]), 
+                                                y = agg_data$ssGSEA[sel], 
+                                                fill = agg_data$Dataset[sel])) +
+      geom_boxplot() +
+      labs(title = paste0("lm22: ", current_type, " (K = ", i, ")"), x = "Subtype", 
+           y = "Abundance") +
+      guides(fill = guide_legend(title="Dataset"))
+    
+    ggsave(filename = paste0(output_dir, '/', current_type, '_k=', i, '.png'),
+           plot = last_plot())
   }
 }
